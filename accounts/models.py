0d2b621f2ee1ea80471a214644ac4453519f6ced
Nate <nl2587@nyu.edu>
2025-02-28 08:30:07 -0500

    can only post a spot with verification

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
+# accounts/models.py
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
