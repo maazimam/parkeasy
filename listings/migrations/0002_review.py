20fad3859b0955a0a7ef1f36a1073b72de37d7a8
Maaz Imam <syedmaazimam24@gmail.com>
2025-03-03 13:49:01 -0500

    Fixed past date booking validation in BookingForm

--- listings/migrations/0002_review.py
+++ listings/migrations/0002_review.py
@@ -6,7 +6,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("booking", "0001_initial"),
         ("listings", "0001_initial"),
