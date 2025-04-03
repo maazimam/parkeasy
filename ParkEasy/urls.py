7be1caab74c04783e5ed1b042f212959f05027a1
Junior Garcia <junior.f.garcia99@gmail.com>
2025-04-03 21:10:56 +0500

    remove analytics

--- ParkEasy/urls.py
+++ ParkEasy/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     path("listings/", include("listings.urls")),
     path("booking/", include("booking.urls")),
     path("messaging/", include("messaging.urls")),
-    path("analytics/", include("analytics.urls", namespace="analytics")),
 ]
