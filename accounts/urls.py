0e93d01412ec4b92b52b9334a107950ff653ee94
GladwinO <gladwinosakwe@gmail.com>
2025-04-04 00:51:59 -0400

    password changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from .views import register, user_login, user_logout, verify, profile_view
+from . import views
 
 urlpatterns = [
     path("register/", register, name="register"),
@@ -7,4 +9,13 @@ urlpatterns = [
     path("logout/", user_logout, name="logout"),
     path("verify/", verify, name="verify"),
     path("profile/", profile_view, name="profile"),
+    
+    # Password change URLs
+    path('password_change/', auth_views.PasswordChangeView.as_view(
+        template_name='accounts/password_change.html',
+        success_url='/accounts/password_change_done/'
+    ), name='password_change'),
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='accounts/password_change_done.html'
+    ), name='password_change_done'),
 ]
